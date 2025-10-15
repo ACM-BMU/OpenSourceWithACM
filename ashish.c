15a08d54cd483d5854e01f7b5066082b6eb38b31
ashishagrawalcode <agrawalashish815@gmail.com>
2025-10-15 16:48:17 +0530

    Updated Inroduction

--- ashish.c
+++ ashish.c
@@ -2,4 +2,6 @@
 
 int main() {
     printf("Hello, ACM! Ashish Here!!!\n");
+    printf("I am a B.Tech Computer Science student from Batch 2025-2029.\n");
+    printf("I am passionate about programming and problem-solving.\n");
 }
